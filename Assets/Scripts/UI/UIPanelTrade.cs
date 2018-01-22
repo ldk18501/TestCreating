6db0ce12e009385b993b4db619b59d722d320de7
lvdk <lvdk@bie-plc.com>
2018-01-22 22:33:34 +0800

    add selectable item

--- Assets/Scripts/UI/UIPanelTrade.cs
+++ Assets/Scripts/UI/UIPanelTrade.cs
@@ -7,10 +7,17 @@ using smallone;
 
 public class UIPanelTrade : UIPanel
 {
+    public GameObject objBubble;
+    public GameObject[] objSlots;
+
     void OnEnable()
     {
         EventCenter.Instance.RegisterGameEvent("ClosePanel", OnCloseSelf);
         // EventCenter.Instance.RegisterGameEvent("OpenInventory", OnBagClicked);
+        for (int i = 0; i < 3; i++)
+        {
+            objSlots[i].AddMissingComponent<UISelectableItem>().cbSelect = OnSlotSelect;
+        }
     }
 
     void OnDisable()
@@ -22,11 +29,23 @@ public class UIPanelTrade : UIPanel
         }
     }
 
+    protected override void OnPanelShowBegin()
+    {
+        base.OnPanelShowBegin();
+        objBubble.SetActive(false);
+    }
+
     void OnCloseSelf()
     {
         UIPanelManager.Instance.HidePanel("UIPanelTrade").DoOnHideCompleted((panel) =>
         {
             Debug.Log("closed!");
         });
+    }
+
+    void OnSlotSelect(bool isSelect,GameObject obj) {
+        if (objBubble)
+            objBubble.transform.localPosition = obj.transform.localPosition + new Vector3(30, 50);
+        objBubble.SetActive(isSelect);
     }
 }
---
+++ Assets/Scripts/UI/UISelectableItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+public class UISelectableItem : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public System.Action<bool, GameObject> cbSelect;
+
+    //Do this when the selectable UI object is selected.
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name + " was selected");
+        if (cbSelect != null)
+            cbSelect.Invoke(true, gameObject);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name + " was unselected");
+        if (cbSelect != null)
+            cbSelect.Invoke(false, gameObject);
+    }
+}
